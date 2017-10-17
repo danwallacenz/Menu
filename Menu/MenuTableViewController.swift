672fd794738597d7efe97562ed0b6f890ed85e86
Dan Wallace <daniel.wallace.nz@gmail.com>
2017-10-17 13:19:11 +1300

    add >1 storyboard

--- Menu/MenuTableViewController.swift
+++ Menu/MenuTableViewController.swift
@@ -10,25 +10,10 @@ import UIKit
 
 class MenuTableViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-    
     @IBAction func dismiss(_ sender: Any) {
         presentingViewController?.dismiss(animated: true, completion: nil)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         guard let menuVC = storyboard?.instantiateViewController(withIdentifier: "MenuItemVC") else { return }
