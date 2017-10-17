5c74a404a49d654ad355859bcf9e723984055bad
Dan Wallace <daniel.wallace.nz@gmail.com>
2017-10-18 11:02:03 +1300

    slideout ok

--- Menu/MenuNavController.swift
+++ Menu/MenuNavController.swift
@@ -10,13 +10,61 @@ import UIKit
 
 class MenuNavController: UINavigationController {
 
+    var interactionController: UIPercentDrivenInteractiveTransition?
     let customTransitionDelegate = SlideInPresentationManager()
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        modalPresentationStyle = .custom
+        transitioningDelegate = customTransitionDelegate
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let panOut = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(handleGesture(_:)))
+        panOut.edges = .left
+        view.addGestureRecognizer(panOut)
+    }
+}
 
-        modalPresentationStyle = .custom
-        transitioningDelegate = customTransitionDelegate
-        // Do any additional setup after loading the view.
+extension MenuNavController: UIGestureRecognizerDelegate {
+    
+    // pan in transitions to next view controller
+    
+    @objc func handleGesture(_ gesture: UIPanGestureRecognizer) {
+        let translate = gesture.translation(in: gesture.view)
+        let percent   = -translate.x / gesture.view!.bounds.size.width
+        
+        if gesture.state == .began {
+            interactionController = UIPercentDrivenInteractiveTransition()
+            customTransitionDelegate.interactionController = interactionController
+            
+            dismiss(animated: true)
+        } else if gesture.state == .changed {
+            interactionController?.update(percent)
+        } else if gesture.state == .ended {
+            let velocity = gesture.velocity(in: gesture.view)
+            interactionController?.completionSpeed = 0.999  // https://stackoverflow.com/a/42972283/1271826
+            if (percent > 0.5 && velocity.x == 0) || velocity.x < 0 {
+                interactionController?.finish()
+            } else {
+                interactionController?.cancel()
+            }
+            interactionController = nil
+        }
+        
+    }
+    
+    // make sure it only recognizes upward gestures
+    
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        if let pan = gestureRecognizer as? UIPanGestureRecognizer {
+            let translation = pan.translation(in: pan.view)
+            let angle = atan2(translation.x, translation.y)
+            return abs(angle + .pi / 2.0) < (.pi / 8.0)
+        }
+        return false
     }
 }
--- Menu/SuperViewController.swift
+++ Menu/SuperViewController.swift
@@ -10,16 +10,47 @@ import UIKit
 
 class SuperViewController: UIViewController {
 
+    var interactionController: UIPercentDrivenInteractiveTransition?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let panOut = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(handleGesture(_:)))
+        panOut.edges = .left
+        view.addGestureRecognizer(panOut)
     }
-
-
+    
     @IBAction func showMenu(_ sender: Any) {
+        presentSideMenu()
+    }
+    
+    private func presentSideMenu() {
         let menuStoryboard = UIStoryboard(name: "Menu", bundle: Bundle.main)
-        let menuVC = menuStoryboard.instantiateViewController(withIdentifier: "MenuVC")
+        guard let menuVC = menuStoryboard.instantiateViewController(withIdentifier: "MenuVC") as? MenuNavController else { return }
+        
+        interactionController = UIPercentDrivenInteractiveTransition()
+        menuVC.customTransitionDelegate.interactionController = interactionController
+        
         present(menuVC, animated: true, completion: nil)
+    }
+    
+    @objc func handleGesture(_ gesture: UIPanGestureRecognizer) {
+        let translate = gesture.translation(in: gesture.view)
+        let percent   = translate.x / gesture.view!.bounds.size.width
+        
+        if gesture.state == .began {
+            self.presentSideMenu()
+        } else if gesture.state == .changed {
+            interactionController?.update(percent)
+        } else if gesture.state == .ended || gesture.state == .cancelled {
+            let velocity = gesture.velocity(in: gesture.view)
+            interactionController?.completionSpeed = 0.999  // https://stackoverflow.com/a/42972283/1271826
+            if (percent > 0.5 && velocity.x == 0) || velocity.x > 0 {
+                interactionController?.finish()
+            } else {
+                interactionController?.cancel()
+            }
+            interactionController = nil
+        }
     }
 }
